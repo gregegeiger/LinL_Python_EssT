64b2b4352206100ed56a618d32510ef5e6fb6cb7
Greg Geiger <greg.e.geiger@gmail.com>
2020-05-16 11:56:19 -0500

    Change operator to modulo

--- Chap03/types.py
+++ Chap03/types.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
+#
+# 2020-04-19:geg
+# demo of difference in float vs decimal
+#
+# You don't want to use floating point for money - instead use the
+#
 
-x = 7
+from decimal import *
+
+a = Decimal("0.10")
+
+b = Decimal("0.30")
+
+# x = .1 + .1 + .1 - .3
+
+x = a + a + a - b
+
 print('x is {}'.format(x))
 print(type(x))
